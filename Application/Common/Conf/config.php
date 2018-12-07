8a03dc4ea9c5f23b8a2551adf69af7adc99e0889
LIU-D <943445214@qq.com>
2018-12-07 18:35:58 +0800

    inital

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,8 +6,8 @@ return array(
 //    'DEFAULT_ACTION'        =>  'index', // 默认操作名称
 
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'www.liu.com', // 服务器地址
-    'DB_NAME'               =>  'sztqd',          // 数据库名
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'dashou',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '741258o',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
